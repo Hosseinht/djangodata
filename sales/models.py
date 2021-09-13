43d4c749bbda9eaf94c4bf98389d44ff67e9319d
Ryan Taheri <sailantht@gmail.com>
2021-09-13 16:05:35 +0430

    8.Sale Views

--- sales/models.py
+++ sales/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.shortcuts import reverse
 from django.utils import timezone
 from products.models import Product
 from customers.models import Customer
@@ -34,6 +35,9 @@ class Sale(models.Model):
 
     def __str__(self):
         return f"Sales for the amount of ${self.total_price}"
+
+    def get_absolute_url(self):
+        return reverse('sales:detail', kwargs={'pk': self.pk})
 
     def save(self, *args, **kwargs):
         if self.transaction_id == '':
