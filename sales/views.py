c075d0f3909c1977c602f4e917a4a50cf3ade668
Ryan Taheri <sailantht@gmail.com>
2021-09-14 10:04:44 +0430

    8.SearchFrom, Querset & DataFrame

--- sales/views.py
+++ sales/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+import pandas as pd
 from .models import Sale
+from .forms import SalesSearchForm
 
 
 def home_view(request):
-    return render(request, 'sales/home.html', {})
+    form = SalesSearchForm(request.POST or None)  # without or None it get executed automatically
+
+    if request.method == "POST":
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+
+    qs = Sale.objects.filter(created__date=date_from)
+    obj = Sale.objects.get(id=1)
+    df1 = pd.DataFrame(qs.values())
+
+    context = {'form': form}
+    return render(request, 'sales/home.html', context)
 
 
 class SaleListView(ListView):
