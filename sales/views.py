71049efafb5bd345a8b1266f2308e66c2b9a7105
Ryan Taheri <sailantht@gmail.com>
2021-09-15 10:31:23 +0430

    14.GroupedBy

--- sales/views.py
+++ sales/views.py
@@ -10,6 +10,7 @@ def home_view(request):
     sales_df = None
     positions_df = None
     merged_df = None
+    df = None
     form = SalesSearchForm(request.POST or None)  # without or None it get executed automatically
 
     if request.method == "POST":
@@ -25,7 +26,7 @@ def home_view(request):
             sales_df['customer_id'] = sales_df['customer_id'].apply(get_customer_from_id)
             sales_df['salesman_id'] = sales_df['salesman_id'].apply(get_salesman_from_id)
             sales_df['created'] = sales_df['created'].apply(lambda x: x.strftime('%Y-%m-%d'))
-            sales_df.rename({'customer_id': 'Customer', 'salesman_id': 'Salesman', 'id': 'sales_id'}, axis=1,
+            sales_df.rename({'customer_id': 'customer', 'salesman_id': 'salesman', 'id': 'sales_id'}, axis=1,
                             inplace=True)
             # inplace = True or  sales_df=sales_df.rename....
             # axis=1 refer to columns
@@ -42,13 +43,17 @@ def home_view(request):
                     positions_data.append(obj)
             positions_df = pd.DataFrame(positions_data)
             merged_df = pd.merge(sales_df, positions_df, on='sales_id')
+
+            df = merged_df.groupby('transaction_id', as_index=False)['price'].agg('sum')
+
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
             merged_df = merged_df.to_html()
+            df = df.to_html()
         else:
             print('no data')
 
-    context = {'form': form, 'sales_df': sales_df, 'positions_df': positions_df, 'merged_df': merged_df}
+    context = {'form': form, 'sales_df': sales_df, 'positions_df': positions_df, 'merged_df': merged_df, 'df': df}
     return render(request, 'sales/home.html', context)
 
 
